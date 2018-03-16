16136c6c9321118cbd456ac7cfbbf05ae98212d4
Michal Konarski <michal.konarski@u2i.com>
2018-03-16 21:16:10 +0000

    Add click handler

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
     render() {
         return (
             <div className="App">
@@ -13,67 +12,93 @@ class App extends Component {
 
 }
 
+class Game extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+
+        const BOARD_SIZE = 10
+
+        var boardState = new Array(BOARD_SIZE);
+        for (var i = 0; i < BOARD_SIZE; i++) {
+            boardState[i] = new Array(BOARD_SIZE);
+        }
+
+        boardState[0][0] = 'X';
+        boardState[3][3] = 'O';
+
+        this.state = {
+            boardState: boardState
+        }
+    }
+
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board size={10} boardState={this.state.boardState}/>
+                </div>
+                <div className="game-info">
+                    Current player:
+                </div>
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                {
+                    Array(this.props.size)
+                        .fill(null)
+                        .map((_, i) => <Row key={`row${i}`} row={i} size={this.props.size} rowState={this.props.boardState[i]}/>)
+                }
+            </div>
+        );
+    }
+}
+
 class Row extends React.Component {
+    createCell(row, col, value) {
+        return (
+            <Cell key={`cell${row}_${col}`} col={col} row={row} value={value} />
+        )
+    }
+
     render() {
         return (
             <div className="board-row">
                 {
                     Array(this.props.size)
                         .fill(null)
-                        .map((_, i) => <Square column={i} row={this.props.row} />)
+                        .map((_, col) => this.createCell(this.props.row, col, this.props.rowState[col]) )
                 }
             </div>
         );
     }
 }
 
-class Square extends React.Component {
+class Cell extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        console.log(`row = ${this.props.row} col = ${this.props.col}`)
+    }
+
     render() {
         return (
-            <button className="square">
-                X
+            <button className="cell" onClick={this.handleClick}>
+                {this.props.value}
             </button>
         );
     }
 }
-
-class Board extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    renderSquare(i) {
-        return <Square value={i} />;
-    }
-
-    render() {
-        return (
-            <div>
-                {
-                    Array(this.props.size)
-                    .fill(null)
-                    .map((_, i) => <Row row={i} size={this.props.size} />)
-                }
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board size={10} />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
 
 export default App;
